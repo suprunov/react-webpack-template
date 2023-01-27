52b6ff2a51fa7e77c018bf3e81195da90ab0ce08
Sergey Suprunov <sergeysuprunov@gmail.com>
2023-01-27 23:08:17 +0300

    init

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM node:latest
+
+WORKDIR /barebonesReact
+
+CMD tail -f /dev/null
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {hot} from 'react-hot-loader/root';
+
+function App() {
+  return (
+    <>
+      <div>Hello World</div>
+      <div>Happy coding</div>
+    </>
+  );
+}
+
+export default hot(App);
